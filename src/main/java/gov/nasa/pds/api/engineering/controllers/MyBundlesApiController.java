316a62510703c4d3f2deeb567ec59a7c49a3ac89
Al Niessner <Al.Niessner@xxx.xxx>
2021-05-12 16:54:40 -0700

    now get products of a bundle. works and tested

--- src/main/java/gov/nasa/pds/api/engineering/controllers/MyBundlesApiController.java
+++ src/main/java/gov/nasa/pds/api/engineering/controllers/MyBundlesApiController.java
@@ -245,28 +245,32 @@ public class MyBundlesApiController extends MyProductsApiBareController implemen
 	    	if (getBundleResponse.isExists())
 	    	{
 	    		MyBundlesApiController.log.info("get response " + getBundleResponse.toString());
-				List<String> collections = (List<String>) getBundleResponse.getSourceAsMap().get("ref_lid_product");
+				List<String> collections = (List<String>) getBundleResponse.getSourceAsMap().get("ref_lid_collection");
         		String collectionLidVid;
 
         		for (String collectionLid : collections )
         		{
-        			collectionLidVid = this.getLatestLidVidFromLid(collectionLid);
+        			collectionLidVid = this.getLatestLidVidFromLid(collectionLid) + "::P1";
+        			MyBundlesApiController.log.info("get collection with lidvid " + collectionLidVid);
         			GetRequest getCollectionRequest = new GetRequest(this.esRegistryConnection.getRegistryRefIndex(), collectionLidVid);
         			GetResponse getCollectionResponse = restHighLevelClient.get(getCollectionRequest, RequestOptions.DEFAULT);
 
         			if (getCollectionResponse.isExists())
         			{
-        				MyBundlesApiController.log.info("get response " + getCollectionResponse.toString());
-        				productLidvids.addAll((List<String>) getBundleResponse.getSourceAsMap().get("product_lidvid"));
+        				MyBundlesApiController.log.info("get ref response " + getCollectionResponse.toString());
+        				Object temp = getCollectionResponse.getSourceAsMap().get("product_lidvid");
+        				if (temp instanceof String) { productLidvids.add((String)temp); }
+        				else { productLidvids.addAll((List<String>) temp); }
         			}
         			else
         			{
         				MyBundlesApiController.log.warn("Couldn't get collection child " + collectionLidVid + " of bundle " + lidvid + " in elasticSearch");
         			}
         		}
-
+        		MyBundlesApiController.log.info("total number of product lidvids " + Integer.toString(productLidvids.size()));
         		for (int i=start ;  i < start+limit && i < productLidvids.size() ;  i++)
-        		{        				
+        		{
+        			MyBundlesApiController.log.info("fetch product from lidvid " + productLidvids.get(i));
         			GetRequest getProductRequest = new GetRequest(this.esRegistryConnection.getRegistryIndex(), productLidvids.get(i));
         			GetResponse getProductResponse = restHighLevelClient.get(getProductRequest, RequestOptions.DEFAULT);
         			
--- verify/issue_56.py
+++ verify/issue_56.py
@@ -8,7 +8,7 @@ import requests
 
 TEST_N_CRITERIA=[
     ('http://localhost:8080/bundles/urn:nasa:pds:izenberg_pdart14_meap::1.0/collections',200,4),  #57
-    ('http://localhost:8080//bundles/urn:nasa:pds:izenberg_pdart14_meap::1.0/products',200,999),  #59
+    ('http://localhost:8080//bundles/urn:nasa:pds:izenberg_pdart14_meap::1.0/products',200,12),  #59
     ('http://localhost:8080/collections/urn:nasa:pds:izenberg_pdart14_meap:data_eetable::1.0/bundles',200,999),  #62
     ('http://localhost:8080/collections/urn:nasa:pds:izenberg_pdart14_meap:data_eetable::1.0/products',200,3),  #58
     ('http://localhost:8080/products/urn:nasa:pds:izenberg_pdart14_meap:data_eetable:ele_evt_8hr_orbit_2012-2013::1.0/bundles',200,999),  #60
