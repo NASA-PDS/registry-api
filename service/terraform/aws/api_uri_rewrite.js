58a4ad14eb49f37e26b14534e84a650b17fdc8ba
Jimmie Young <jdyoung@jpl.nasa.gov>
2022-02-23 17:35:15 -0800

    Updates to CF Function based on Eddie's code review.

--- service/terraform/aws/api_uri_rewrite.js
+++ service/terraform/aws/api_uri_rewrite.js
@@ -8,30 +8,30 @@ to:
 
 // The function is in response to the 'viewer request' event
 function handler(event) {
-    var request = event.request;
-    var incoming_uri = request.uri;
+    let request = event.request;
+    let incomingUri = request.uri;
  
     // split the uri
-    const uri_parts = incoming_uri.split("/");
+    const uriParts = incomingUri.split("/");
  
     // continue with the rewrite only if "/api/" is first in the URI
-    if (uri_parts[1].toLowerCase() == "api") {
+    if (uriParts[1].toLowerCase() == "api") {
  
         // at a minimum service and version are required, otherwise fall through w/o changes
-        if (uri_parts.length >= 4) {
+        if (uriParts.length >= 4) {
             // extract service and version which are placed in the HTTP header
-            const result_header = uri_parts[2] + "/" + uri_parts[3];
+            const resultHeader = uriParts[2] + "/" + uriParts[3];
  
             // copy the rest of the request path to the new URI
-            var new_uri = "";
-            for(let i = 4; i < uri_parts.length; i++) {
-                new_uri += "/" + uri_parts[i];
+            let newUri = "";
+            for(let i = 4; i < uriParts.length; i++) {
+                newUri += "/" + uriParts[i];
             }
  
             // write the updated URI and the new HTTP header to the request only
             // if things check out to this point
-            request.uri = new_uri;
-            request.headers['x-request-node'] = result_header;
+            request.uri = newUri;
+            request.headers['x-request-node'] = resultHeader;
         }
  
     }
