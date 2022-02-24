69661ff94362c2f1ef6162c4f66a583ab0c00a5b
Jimmie Young <jdyoung@jpl.nasa.gov>
2022-02-23 17:12:01 -0800

    Revise handler logic after finding out the incoming uri doesn't include protocol and host.

--- service/terraform/aws/api_uri_rewrite.js
+++ service/terraform/aws/api_uri_rewrite.js
@@ -1,8 +1,8 @@
 /*
 Cloudfront Function which performs URL rewrite to direct requests of the form:
-    https://<host>/api/<service>{-node}/<version>/<request>
+    /api/<service>{-node}/<version>/<request>
 to:
-    https://<host>/<request>
+    /<request>
     + the http header 'x-request-node:<service>{-node}/<version>'
 */
 
@@ -11,25 +11,21 @@ function handler(event) {
     var request = event.request;
     var incoming_uri = request.uri;
  
-    // split the uri on "/api/" - only api invocations are rewritten by this function
-    const uri_parts = incoming_uri.split("/api/");
+    // split the uri
+    const uri_parts = incoming_uri.split("/");
  
-    // continue with the rewrite only if "/api/" is in the URI
-    if (uri_parts.length > 1) {
-        // save off uri prefix - typically protocol and host
-        var new_uri = uri_parts[0];
- 
-        // decompose the URI path
-        const path_parts = uri_parts[1].split("/");
+    // continue with the rewrite only if "/api/" is first in the URI
+    if (uri_parts[1].toLowerCase() == "api") {
  
         // at a minimum service and version are required, otherwise fall through w/o changes
-        if (path_parts.length >= 2) {
+        if (uri_parts.length >= 4) {
             // extract service and version which are placed in the HTTP header
-            const result_header = path_parts[0] + "/" + path_parts[1];
+            const result_header = uri_parts[2] + "/" + uri_parts[3];
  
             // copy the rest of the request path to the new URI
-            for(let i = 2; i < path_parts.length; i++) {
-                new_uri += "/" + path_parts[i];
+            var new_uri = "";
+            for(let i = 4; i < uri_parts.length; i++) {
+                new_uri += "/" + uri_parts[i];
             }
  
             // write the updated URI and the new HTTP header to the request only
