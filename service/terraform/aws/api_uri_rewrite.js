a6250e07c7601c0eec456b14b17089e8795d4702
Jimmie Young <jdyoung@jpl.nasa.gov>
2022-02-25 14:49:23 -0800

    Fix value specification of x-request-node header, changes to comply w/ javascript version for CF functions (e.g. let/const -> var)

--- service/terraform/aws/api_uri_rewrite.js
+++ service/terraform/aws/api_uri_rewrite.js
@@ -3,38 +3,38 @@ Cloudfront Function which performs URL rewrite to direct requests of the form:
     /api/<service>{-node}/<version>/<request>
 to:
     /<request>
-    + the http header 'x-request-node:<service>{-node}/<version>'
+    + the http header json : { "x-request-node" : { "value" : "<service>{-node}/<version>" }
 */
 
 // The function is in response to the 'viewer request' event
 function handler(event) {
-    let request = event.request;
-    let incomingUri = request.uri;
- 
+    var request = event.request;
+    var incomingUri = request.uri;
+
     // split the uri
-    const uriParts = incomingUri.split("/");
- 
+    var uriParts = incomingUri.split("/");
+
     // continue with the rewrite only if "/api/" is first in the URI
     if (uriParts[1].toLowerCase() == "api") {
- 
+
         // at a minimum service and version are required, otherwise fall through w/o changes
         if (uriParts.length >= 4) {
             // extract service and version which are placed in the HTTP header
-            const resultHeader = uriParts[2] + "/" + uriParts[3];
- 
+            var resultHeader = uriParts[2] + "/" + uriParts[3];
+
             // copy the rest of the request path to the new URI
-            let newUri = "";
-            for(let i = 4; i < uriParts.length; i++) {
+            var newUri = "";
+            for(var i = 4; i < uriParts.length; i++) {
                 newUri += "/" + uriParts[i];
             }
- 
+
             // write the updated URI and the new HTTP header to the request only
             // if things check out to this point
             request.uri = newUri;
-            request.headers['x-request-node'] = resultHeader;
+            request.headers['x-request-node'] = { "value" : resultHeader };
         }
- 
+
     }
- 
+
     return request;
 }
