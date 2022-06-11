66861aebb9bbcba393957707d2374a60fe667714
Al Niessner <Al.Niessner@xxx.xxx>
2022-06-11 14:49:49 -0700

    fix for 141

--- service/src/main/java/gov/nasa/pds/api/registry/configuration/WebMVCConfig.java
+++ service/src/main/java/gov/nasa/pds/api/registry/configuration/WebMVCConfig.java
@@ -42,9 +42,6 @@ public class WebMVCConfig implements WebMvcConfigurer
 	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry)
 	{
-		registry.addResourceHandler("swagger-ui.html")
-		.addResourceLocations("classpath:/META-INF/resources/");
-
 		registry.addResourceHandler("/webjars/**")
 		.addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
