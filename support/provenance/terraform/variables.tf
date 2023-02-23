1cb9fb3d252ab738b688edf11ae7a250c372c3aa
Jimmie Young <jimmie.d.young@gmail.com>
2023-02-23 13:01:00 -0800

    Scheduled execution of provenance script (#253)
    * Add debug output of identified LIDVIDs
    
    * Initial import of python driver for provenance for use in docker images.
    
    * Initial import of dockerfile to create executable provenance image for AWS.
    
    * Add copyright header.
    
    * Add python shell declaration to enable stand-alone execution.
    
    * Fix CMD in dockerfile
    
    * Format change.
    
    * Code and logic cleanup
    
    * Change name of endpoint var
    
    * Add pip install of remotes to dockerfile.
    
    * Add .terraform directory to .gitignore
    
    * Initial version of terraform scripts to deploy scheduled provenance execution.
    
    * Update swagger based healthcheck.
    
    ---------
    
    Co-authored-by: Jimmie Young <jdyoung@jpl.nasa.gov>

---
+++ support/provenance/terraform/variables.tf
@@ -0,0 +1,65 @@
+variable "aws_account_id" {
+  description = "AWS Account Id"
+  default = "445837347542"
+}
+
+variable "node_name_abbr" {
+  description = "Node name abbreviation"
+  default = "en"
+}
+
+variable "venue" {
+  description = "Deployment venue (prod, test, dev)"
+  default = "dev"
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  default = "us-west-2"
+}
+
+variable "aws_shared_credentials_file" {
+  description = "AWS shared credentials file"
+  default = "~/.aws/credentials"
+}
+
+variable "aws_profile" {
+  description = "AWS profile"
+  default = "default"
+}
+
+variable "aws_fg_vpc" {
+  description = "AWS VPC for Fargate"
+}
+
+variable "aws_fg_security_groups" {
+  description = "AWS Security groups for Fargate"
+  type = list(string)
+}
+
+variable "aws_fg_subnets" {
+  description = "AWS Subnets for Fargate"
+  type = list(string)
+}
+
+variable "prov_endpoint" {
+  description = "Opensearch endpoint (full URL) which to connect."
+}
+
+variable "prov_remote_lists" {
+  description = "JSON list containing strings of space separated ES remotes"
+}
+
+variable "aws_ecr_repository" {
+  description = "Name of the AWS ECR Repository"
+  default = "pds-registry-api-service"
+}
+
+variable "aws_fg_image" {
+  description = "AWS image name for Fargate"
+}
+
+variable "aws_ecr_pull_policy_arn" {
+  description = "ARN for the ECR pull policy"
+  default = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+}
