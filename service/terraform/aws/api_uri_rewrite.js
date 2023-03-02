cfde54e2e40c05a08a068e591363068cd395389d
Jimmie Young <jimmie.d.young@gmail.com>
2023-03-02 10:02:28 -0800

    Add logging messages to CF api rewrite function. (#264)
    * Add logging messages to CF api rewrite function.
    
    * Add conditional logging based on presence of cf_debug query parameter
    
    * add comments about the qualifications for rewrite per PR suggestion
    
    * Remove magic string in place of ersatz-constant
    
    ---------
    
    Co-authored-by: Jimmie Young <jdyoung@jpl.nasa.gov>

--- service/terraform/aws/api_uri_rewrite.js
+++ service/terraform/aws/api_uri_rewrite.js
@@ -14,45 +14,73 @@ var VER_IDX = 3
 var CMD_IDX = 4
 
 var TARGET_URI_PREFIX = "/api/search"
+var DEBUG_PARAM       = "cf_debug" 
 
 // The function is in response to the 'viewer request' event
 function handler(event) {
     var request = event.request;
     var incomingUri = request.uri;
 
+    // See if detailed logging is enabled
+    var debug = false
+    if (DEBUG_PARAM in request.querystring) {
+         debug = true
+         delete request.querystring[DEBUG_PARAM]
+    }
+
     // Strip out any leading multiple slashies - registry-api#208
     while (incomingUri.indexOf("//") >= 0) {
         incomingUri = incomingUri.replace("//","/");
     }
+    
     // Put the URI back into the request now in case the rewrite doesn't happen
     request.uri = incomingUri
             
+    if (debug) console.log("incoming URI [" + incomingUri + "]")
+
     // continue with the rewrite only if "/api/search*" is first in the URI
     if (incomingUri.startsWith(TARGET_URI_PREFIX)) {
 
-        // split the uri
+        if (debug) console.log("incoming URI matches prefix [" + TARGET_URI_PREFIX + "]")
+
+        // split the uri - note that w/ a initial '/' the first element in the resulting
+        // array will be empty, so be sure to consider this when computing indexes of
+        // each token
         var uriParts = incomingUri.split("/");
 
-        // at a minimum service, version and command are required, otherwise fall through w/o changes
+        // at a minimum service, version and command are required (i.e. there are non-empty components in the URI after
+        // "/api/search/<version>") in order consider a rewrite of the URI, otherwise fall through w/o changes
         if (uriParts.length > CMD_IDX && uriParts[CMD_IDX].trim() != "") {
+            if (debug) console.log("incoming URI includes a command")
+
             // ensure that the API version is major version only
             var reqVer = uriParts[VER_IDX];
             var majorVer = reqVer.split(".")[0];
+            if (debug) console.log("revised request version [" + reqVer + "] to [" + majorVer + "]")
             
             // extract service and version which are placed in the HTTP header
             var resultHeader = uriParts[SVC_IDX] + "/" + majorVer;
+            if (debug) console.log("x-request-node header value [" + resultHeader + "]")
             
             // copy the rest of the request path to the new URI
             var newUri = "";
             for(var i = CMD_IDX; i < uriParts.length; i++) {
                 newUri += "/" + uriParts[i];
+                if (debug) console.log("appending to URI path [" + uriParts[i] + "]")
             }
 
-            // write the updated URI and the new HTTP header to the request only
-            // if things check out to this point
+            // set updated URI into request and set request node header
             request.uri = newUri;
             request.headers['x-request-node'] = { "value" : resultHeader };
+            if (debug) {
+                console.log("incoming URI [" + incomingUri + "] rewritten to [" + newUri 
+                    + "] & x-request-node [" + resultHeader + "]")
+            }
+        } else {
+            if (debug) console.log("incoming URI [" + incomingUri + "] : no rewrite (no command)")
         }
+    } else {
+        if (debug) console.log("incoming URI [" + incomingUri + "] : no rewrite (no prefix match)")
     }
 
     return request;
