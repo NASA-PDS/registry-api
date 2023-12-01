50de22bdddb5aa3c1e7aa1dc92f9a5cc4f5649e1
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2023-11-30 17:04:19 -0800

    reimplement RefLogicNonAggregateProduct.memberOf() to use ancestry metadata

--- service/src/main/java/gov/nasa/pds/api/registry/model/RefLogicNonAggregateProduct.java
+++ service/src/main/java/gov/nasa/pds/api/registry/model/RefLogicNonAggregateProduct.java
@@ -12,10 +12,13 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import gov.nasa.pds.api.registry.RequestBuildContext;
+import gov.nasa.pds.api.registry.UserContext;
+import gov.nasa.pds.api.registry.exceptions.ApplicationTypeException;
 import gov.nasa.pds.api.registry.model.identifiers.LidVidUtils;
 import gov.nasa.pds.api.registry.model.identifiers.PdsLid;
 import gov.nasa.pds.api.registry.model.identifiers.PdsLidVid;
 import gov.nasa.pds.api.registry.model.identifiers.PdsProductIdentifier;
+import gov.nasa.pds.api.registry.search.QuickSearch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,6 +34,8 @@ import gov.nasa.pds.api.registry.search.RequestBuildContextFactory;
 import gov.nasa.pds.api.registry.search.RequestConstructionContextFactory;
 import gov.nasa.pds.api.registry.search.SearchRequestFactory;
 import gov.nasa.pds.api.registry.util.GroupConstraintImpl;
+
+import static gov.nasa.pds.api.registry.model.identifiers.LidVidUtils.getAllLidVidsByLids;
 
 @Immutable
 class RefLogicNonAggregateProduct extends RefLogicAny implements ReferencingLogic {
@@ -100,5 +105,45 @@ class RefLogicNonAggregateProduct extends RefLogicAny implements ReferencingLogi
     }
 
     return parents;
+  }
+
+  @Override
+  public RequestAndResponseContext memberOf(
+      ControlContext ctrlContext, UserContext searchContext, boolean twoSteps)
+      throws ApplicationTypeException, IOException, LidVidNotFoundException {
+    String ancestorMetadataKey =
+        twoSteps
+            ? "ops:Provenance/ops:parent_bundle_identifier"
+            : "ops:Provenance/ops:parent_collection_identifier";
+    ReferencingLogicTransmuter ancestorClass =
+        twoSteps ? ReferencingLogicTransmuter.Bundle : ReferencingLogicTransmuter.Collection;
+
+    List<String> ancestorIdentifiers =
+        QuickSearch.getValues(
+            ctrlContext.getConnection(), false, searchContext.getLidVid(), ancestorMetadataKey);
+
+    //    Get all the LIDVID refs, resolve the LID refs to all relevant LIDVIDs, then add them all
+    // together
+    Set<String> parentLidvids =
+        ancestorIdentifiers.stream()
+            .filter(PdsProductIdentifier::stringIsLidvid)
+            .collect(Collectors.toSet());
+    List<String> parentLids =
+        ancestorIdentifiers.stream()
+            .filter(PdsProductIdentifier::stringIsLid)
+            .collect(Collectors.toList());
+    List<String> implicitParentLidvids =
+        getAllLidVidsByLids(
+            ctrlContext,
+            RequestBuildContextFactory.given(false, "lid", ancestorClass.impl().constraints()),
+            parentLids);
+    parentLidvids.addAll(implicitParentLidvids);
+
+    GroupConstraint ancestorProductTypeContstraints = ancestorClass.impl().constraints();
+    GroupConstraint ancestorSelectorConstraint =
+        GroupConstraintImpl.buildAny(Map.of("_id", new ArrayList<>(parentLidvids)));
+    ancestorProductTypeContstraints.union(ancestorSelectorConstraint);
+
+    return rrContextFromConstraint(ctrlContext, searchContext, ancestorSelectorConstraint);
   }
 }
