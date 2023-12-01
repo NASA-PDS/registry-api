2ae38ca2f99aeb8d5c70dcfc254060dd197768d5
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2023-11-30 17:15:23 -0800

    excise Pagination and related elements

--- service/src/main/java/gov/nasa/pds/api/registry/model/RefLogicAny.java
+++ service/src/main/java/gov/nasa/pds/api/registry/model/RefLogicAny.java
@@ -27,11 +27,7 @@ class RefLogicAny implements ReferencingLogic {
     return GroupConstraintImpl.empty();
   }
 
-  private boolean isGrandchild(ReferencingLogicTransmuter idType) {
-    return !(idType == ReferencingLogicTransmuter.Bundle
-        || idType == ReferencingLogicTransmuter.Collection);
-  }
-
+//  TODO: Clarify what this does and whether it can be excised --- edunn
   private ReferencingLogicTransmuter resolveID(ControlContext context, UserContext input)
       throws IOException, LidVidNotFoundException, UnknownGroupNameException {
     PdsProductIdentifier productIdentifier = LidVidUtils.resolve(input.getIdentifier(),
@@ -45,36 +41,8 @@ class RefLogicAny implements ReferencingLogic {
       boolean isIdToGroup) throws ApplicationTypeException, IOException, LidVidNotFoundException,
       UnknownGroupNameException {
 
+//    check git blame for previous implementation if this exception is ever encountered --- edunn 20231130
     throw new IOException("DEVELOPMENT THROW - HOPEFULLY THIS ISN'T NECESSARY AND CAN BE EXCISED");
-
-//    // find all of the given group that reference the specified ID
-//    ReferencingLogicTransmuter groupType =
-//        ReferencingLogicTransmuter.getBySwaggerGroup(input.getGroup());
-//    ReferencingLogicTransmuter idType = this.resolveID(context, input);
-//
-//    if (idType == ReferencingLogicTransmuter.Bundle
-//        && groupType == ReferencingLogicTransmuter.Collection)
-//      return RequestAndResponseContext.buildRequestAndResponseContext(context, input,
-//          RefLogicBundle.children(context, input.getSelector(), input));
-//    if (idType == ReferencingLogicTransmuter.Bundle && this.isGrandchild(groupType))
-//      return RequestAndResponseContext.buildRequestAndResponseContext(context, input,
-//          RefLogicBundle.grandchildren(context, input.getSelector(), input));
-//    if (idType == ReferencingLogicTransmuter.Collection
-//        && groupType == ReferencingLogicTransmuter.Bundle)
-//      return RequestAndResponseContext.buildRequestAndResponseContext(context, input,
-//          RefLogicCollection.parents(context, input.getSelector(), input));
-//    if (idType == ReferencingLogicTransmuter.Collection && this.isGrandchild(groupType))
-//      return RequestAndResponseContext.buildRequestAndResponseContext(context, input,
-//          RefLogicCollection.children(context, input.getSelector(), input));
-//    if (this.isGrandchild(idType) && groupType == ReferencingLogicTransmuter.Bundle)
-//      return RequestAndResponseContext.buildRequestAndResponseContext(context, input,
-//          RefLogicNonAggregateProduct.grandparents(context, input.getSelector(), input));
-//    if (this.isGrandchild(idType) && groupType == ReferencingLogicTransmuter.Collection)
-//      return RequestAndResponseContext.buildRequestAndResponseContext(context, input,
-//          RefLogicNonAggregateProduct.parents(context, input.getSelector(), input));
-//
-//    throw new IOException("Waiting on implementation until references are figured out in database");
-//    // FIXME: return isIdToGroup ? this.idToGroup() : this.groupToId();
   }
 
   @Override
--- service/src/main/java/gov/nasa/pds/api/registry/model/RefLogicNonAggregateProduct.java
+++ service/src/main/java/gov/nasa/pds/api/registry/model/RefLogicNonAggregateProduct.java
@@ -48,65 +48,6 @@ class RefLogicNonAggregateProduct extends RefLogicAny implements ReferencingLogi
     return GroupConstraintImpl.buildNot(preset);
   }
 
-  static Pagination<String> grandparents(ControlContext control, ProductVersionSelector selection,
-      LidvidsContext uid) throws IOException, LidVidNotFoundException {
-    log.info("Find the grandparents of a product -- both all and latest");
-    List<String> parents = RefLogicNonAggregateProduct
-        .parents(control, ProductVersionSelector.LATEST, new Unlimited(uid.getLidVid())).page();
-    PaginationLidvidBuilder grandparents = new PaginationLidvidBuilder(uid);
-    for (String parent : parents) {
-      log.info("Find all the parents of collection: " + parent);
-      grandparents
-          .addAll(RefLogicCollection.parents(control, selection, new Unlimited(parent)).page());
-      log.info("Find grandparents size: " + String.valueOf(grandparents.size()));
-      for (String gp : grandparents.page()) {
-        log.info("   grandparent: " + gp);
-      }
-    }
-    return grandparents;
-  }
-
-  static Pagination<String> parents(ControlContext control, ProductVersionSelector selection,
-      LidvidsContext uid) throws IOException, LidVidNotFoundException {
-    List<String> sortedLidStrings;
-    PaginationLidvidBuilder parents = new PaginationLidvidBuilder(uid);
-    Set<String> lids = new HashSet<String>();
-
-    log.info("Find the parents of a product -- both all and latest");
-    for (final Map<String, Object> kvp : new HitIterator(
-        control.getConnection().getRestHighLevelClient(),
-        new SearchRequestFactory(
-            RequestConstructionContextFactory.given("product_lidvid", uid.getLidVid(), true),
-            control.getConnection()).build(RequestBuildContextFactory.given(true, "collection_lid"),
-                control.getConnection().getRegistryRefIndex()))) {
-      lids.addAll(parents.convert(kvp.get("collection_lid")));
-    }
-    sortedLidStrings = new ArrayList<>(lids);
-    Collections.sort(sortedLidStrings);
-    List<PdsProductIdentifier> sortedLids =
-        sortedLidStrings.stream().map(PdsLid::fromString).collect(Collectors.toList());
-
-    if (selection == ProductVersionSelector.ALL) {
-      parents.addAll(LidVidUtils.getAllLidVidsByLids(control, RequestBuildContextFactory.empty(),
-          sortedLidStrings));
-    } else {
-      RequestBuildContext reqContext = RequestBuildContextFactory.empty();
-      for (PdsProductIdentifier id : sortedLids) {
-        try {
-          PdsLidVid latestLidvid =
-              LidVidUtils.getLatestLidVidByLid(control, reqContext, id.getLid().toString());
-          parents.add(latestLidvid.toString());
-        } catch (LidVidNotFoundException e) {
-          log.warn(
-              "LID is referenced but is in non-findable archive-status or does not exist in db: "
-                  + e.toString());
-        }
-      }
-    }
-
-    return parents;
-  }
-
   @Override
   public RequestAndResponseContext memberOf(
       ControlContext ctrlContext, UserContext searchContext, boolean twoSteps)
