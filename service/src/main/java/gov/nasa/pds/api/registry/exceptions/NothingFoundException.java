801d0bfd784a68a78c36508477c133420e05a048
edunn <alexander.e.dunn@jpl.nasa.gov>
2024-08-01 16:15:44 -0700

    glass the repo

--- service/src/main/java/gov/nasa/pds/api/registry/exceptions/NothingFoundException.java
+++
@@ -1,8 +0,0 @@
-package gov.nasa.pds.api.registry.exceptions;
-
-public class NothingFoundException extends Exception {
-  /**
-   * 
-   */
-  private static final long serialVersionUID = -987644388715945160L;
-}
--- service/src/test/resources/POSTMAN_TESTS_README.txt
+++
@@ -1,3 +0,0 @@
-Postman regression tests are currently maintained as part of the registry docker repository
-
-https://github.com/NASA-PDS/registry/tree/main/docker/postman
