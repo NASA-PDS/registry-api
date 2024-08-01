36bec5f7b9d74b60a0393c13569518fcd8eb03fa
edunn <alexander.e.dunn@jpl.nasa.gov>
2024-08-01 16:18:29 -0700

    light glass

--- service/src/main/java/gov/nasa/pds/api/registry/model/exceptions/SortSearchAfterMismatchException.java
+++
@@ -1,8 +0,0 @@
-package gov.nasa.pds.api.registry.model.exceptions;
-
-public class SortSearchAfterMismatchException extends RegistryApiException {
-
-  public SortSearchAfterMismatchException(String detail) {
-    super("Invalid combination of sort and searchAfter values: " + detail);
-  }
-}
