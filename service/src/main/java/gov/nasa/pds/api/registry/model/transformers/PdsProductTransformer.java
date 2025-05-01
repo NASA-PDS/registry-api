87acc4644551a510aeb8e05893d30e018cac2573
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2025-05-01 15:17:09 -0400

    add missing files

---
+++ service/src/main/java/gov/nasa/pds/api/registry/model/transformers/PdsProductTransformer.java
@@ -0,0 +1,96 @@
+package gov.nasa.pds.api.registry.model.transformers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import gov.nasa.pds.api.registry.model.EntityProduct;
+import gov.nasa.pds.api.registry.model.Pds4ProductFactory;
+import gov.nasa.pds.api.registry.model.RawMultipleProductResponse;
+import gov.nasa.pds.api.registry.model.SearchUtil;
+import gov.nasa.pds.api.registry.model.properties.PdsProperty;
+import gov.nasa.pds.api.registry.search.OpenSearchFields;
+import gov.nasa.pds.model.Pds4Product;
+import gov.nasa.pds.model.Pds4Products;
+import gov.nasa.pds.model.PdsProduct;
+import gov.nasa.pds.model.PdsProducts;
+import gov.nasa.pds.model.Summary;
+
+public class PdsProductTransformer extends ResponseTransformerImpl {
+
+  private static final Logger log = LoggerFactory.getLogger(PdsProductTransformer.class);
+
+  protected boolean isJSON;
+  protected static final List<String> REQUIRED_FIELDS =
+      List.of(PdsProperty.LIDVID, PdsProperty.TITLE, PdsProperty.PRODUCT_CLASS, PdsProperty.START_DATE_TIME, PdsProperty.STOP_DATE_TIME, PdsProperty.MODIFICATION_DATE, PdsProperty.CREATION_DATE_TIME, PdsProperty.REF_LID_INSTRUMENT_HOST, PdsProperty.REF_LID_INSTRUMENT, PdsProperty.REF_LID_INVESTIGATION, PdsProperty.REF_LID_TARGET, PdsProperty.VID, PdsProperty.DATA_FILE_REF, PdsProperty.TRACK_META_ARCHIVE_STATUS);
+
+
+  private static final List<String> EXCLUDED_PROPERTIES = List.of(PdsProperty.XML_BLOB, PdsProperty.JSON_BLOB);
+
+  @Override
+  public List<String> getRequestedFields(List<String> userRequestFields) {
+    if (userRequestFields != null && !userRequestFields.isEmpty()) {
+      List<String> allFields = new ArrayList<>(REQUIRED_FIELDS);
+      allFields.addAll(userRequestFields);
+      return new ArrayList<>(new TreeSet<>(allFields));
+    }
+    else {
+      // if the user did not specify anything, everything will be returned
+      // we want to keep it like that
+      return null;
+    }
+  }
+
+  @Override
+  @LogExecutionTime
+  public Object transform(RawMultipleProductResponse input, List<String> fields) {
+    log.debug("transform: fields: {}, excluded fields: {}", fields, EXCLUDED_PROPERTIES);
+    PdsProducts products = new PdsProducts();
+    Set<String> uniqueProperties = new TreeSet<String>();
+    Map<String, List<String>> properties;
+
+    for (Map<String, Object> kvp : input.getProducts()) {
+      try {
+        PdsProduct product = SearchUtil.entityProductToAPIProduct(objectMapper.convertValue(kvp, EntityProduct.class), this.baseURL);
+        
+        // TODO check why every value is a String
+        properties = (Map<String, List<String>>) getFilteredProperties(kvp, fields, EXCLUDED_PROPERTIES);
+
+        product.setProperties(properties);
+
+        uniqueProperties.addAll(properties.keySet());
+
+        products.addDataItem(product);
+      } catch (Throwable t) {
+        String lidvid = (String) kvp.getOrDefault("lidvid", "unknown");
+        log.error("DATA ERROR: could not convert opensearch document to EntityProduct for lidvid: "
+            + lidvid, t);
+      }
+    }
+
+    Summary summary = input.getSummary();
+    summary.setProperties(new ArrayList<String>(uniqueProperties));
+    products.setSummary(summary);
+
+    return products;
+  }
+
+  @Override
+  @LogExecutionTime
+  public Object transform(Map<String, Object> kvp, List<String> fields) {
+    EntityProduct ep = objectMapper.convertValue(kvp, EntityProduct.class);
+    PdsProduct product = SearchUtil.entityProductToAPIProduct(ep, this.baseURL);
+    
+    Map<String, List<String>> properties = (Map<String, List<String>>) getFilteredProperties(kvp, fields, EXCLUDED_PROPERTIES);
+    // TODO: findout why the getFilteredProperties method is used here. Should we add fields as a
+    // second argument instead of null ?
+
+    product.setProperties(properties);
+    return product;
+  }
+
+}
---
+++ service/src/main/java/gov/nasa/pds/api/registry/model/transformers/WyriwygProductTransformer.java
@@ -0,0 +1,106 @@
+package gov.nasa.pds.api.registry.model.transformers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import gov.nasa.pds.api.registry.exceptions.UnsupportedSearchProperty;
+import gov.nasa.pds.api.registry.model.EntityProduct;
+import gov.nasa.pds.api.registry.model.Pds4ProductFactory;
+import gov.nasa.pds.api.registry.model.RawMultipleProductResponse;
+import gov.nasa.pds.api.registry.model.SearchUtil;
+import gov.nasa.pds.api.registry.model.properties.PdsProperty;
+import gov.nasa.pds.api.registry.search.OpenSearchFields;
+import gov.nasa.pds.api.registry.util.LogExecutionTime;
+import gov.nasa.pds.model.Pds4Product;
+import gov.nasa.pds.model.Pds4Products;
+import gov.nasa.pds.model.PdsProduct;
+import gov.nasa.pds.model.PdsProducts;
+import gov.nasa.pds.model.Summary;
+import gov.nasa.pds.model.WyriwygProduct;
+import gov.nasa.pds.model.WyriwygProductKeyValuePair;
+import gov.nasa.pds.model.WyriwygProducts;
+
+public class WyriwygProductTransformer extends ResponseTransformerImpl {
+
+  private static final Logger log = LoggerFactory.getLogger(PdsProductTransformer.class);
+
+  protected boolean isJSON;
+  
+
+  private static final List<String> EXCLUDED_PROPERTIES = List.of(PdsProperty.XML_BLOB, PdsProperty.JSON_BLOB);
+
+  private WyriwygProduct generateWyriwygProduct(Map<String, Object> hit,
+  List<String> included_fields) {
+  WyriwygProduct product = new WyriwygProduct();
+  String jsonProperty;
+  for (Entry<String, Object> pair : hit.entrySet()) {
+    WyriwygProductKeyValuePair kvp = new WyriwygProductKeyValuePair();
+    try {
+      jsonProperty = SearchUtil.openPropertyToJsonProperty(pair.getKey());
+      if (!EXCLUDED_PROPERTIES.contains(jsonProperty)) {
+        if (included_fields == null || included_fields.isEmpty() || included_fields.contains(jsonProperty)) {
+          kvp.setKey(jsonProperty);
+          kvp.setValue(getStringValueOf(pair.getValue()));
+          product.addKeyValuePairsItem(kvp);
+        }
+      }
+    } catch (UnsupportedSearchProperty e) {
+      log.warn("openSearch property " + pair.getKey() + " is not supported, ignored");
+    }
+
+}
+
+return product;
+
+
+}
+
+private String getStringValueOf(Object o) {
+  String valueOf;
+  if (o instanceof Iterable) {
+    List<String> stringRepresentations = new ArrayList<>();
+    for (Object el : (Iterable<Object>) o) {
+      stringRepresentations.add(String.valueOf(el));
+    }
+
+    String delimiter = "|";
+    valueOf = String.join(delimiter, stringRepresentations);
+  } else {
+    valueOf = String.valueOf(o);
+  }
+
+  return valueOf;
+}
+
+  @Override
+  @LogExecutionTime
+  public Object transform(RawMultipleProductResponse input, List<String> fields) {
+    Set<String> uniqueProperties = new TreeSet<String>();
+    WyriwygProducts products = new WyriwygProducts();
+
+    for (Map<String, Object> hit : input.getProducts()) {
+      uniqueProperties.addAll(getFilteredProperties(hit, fields, EXCLUDED_PROPERTIES).keySet());
+      WyriwygProduct product = generateWyriwygProduct(hit, fields);
+      products.addDataItem(product);
+    }
+
+    Summary summary = input.getSummary();
+    summary.setProperties(new ArrayList<String>(uniqueProperties));
+    products.setSummary(summary);
+    return products;
+  }
+
+  @Override
+  @LogExecutionTime
+  public Object transform(Map<String, Object> kvp, List<String> fields) {
+    return generateWyriwygProduct(kvp, fields);
+  }
+
+}
